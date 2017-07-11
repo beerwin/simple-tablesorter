7f4f0e91a8606ba15b72e25d622cf289634d7b8d
Ervin Nagy <e.nagy@metrixlab.com>
2017-07-11 10:46:05 +0300

    Changed the sorter to behave like built-in sorters

--- Src/tablesort.js
+++ Src/tablesort.js
@@ -15,39 +15,39 @@ var tableSort = function(options) {
 	// For a deep extend, set the first argument to `true`.
 	instance.extend = function () {
 
-			// Variables
-			var extended = {};
-			var deep = false;
-			var i = 0;
-			var length = arguments.length;
+		// Variables
+		var extended = {};
+		var deep = false;
+		var i = 0;
+		var length = arguments.length;
 
-			// Check if a deep merge
-			if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-					deep = arguments[0];
-					i++;
-			}
+		// Check if a deep merge
+		if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+			deep = arguments[0];
+			i++;
+		}
 
-			// Merge the object into the extended object
-			var merge = function (obj) {
-					for ( var prop in obj ) {
-							if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
-									// If deep merge and property is an object, merge properties
-									if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
-											extended[prop] = extend( true, extended[prop], obj[prop] );
-									} else {
-											extended[prop] = obj[prop];
-									}
-							}
+		// Merge the object into the extended object
+		var merge = function (obj) {
+			for ( var prop in obj ) {
+				if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+					// If deep merge and property is an object, merge properties
+					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+						extended[prop] = extend( true, extended[prop], obj[prop] );
+					} else {
+						extended[prop] = obj[prop];
 					}
-			};
-
-			// Loop through each object and conduct a merge
-			for ( ; i < length; i++ ) {
-					var obj = arguments[i];
-					merge(obj);
+				}
 			}
+		};
 
-			return extended;
+		// Loop through each object and conduct a merge
+		for ( ; i < length; i++ ) {
+			var obj = arguments[i];
+			merge(obj);
+		}
+
+		return extended;
 
 	};
 	
@@ -77,9 +77,9 @@ var tableSort = function(options) {
 		switch (direction) {
 			default: 
 			case "asc": 
-				return l > r ? 1 : -1;
+				return l > r ? 1 : l < r ? -1 : 0;
 			case "desc":
-				return l < r ? 1 : -1;
+				return l < r ? 1 : l > r ? -1 : 0;
 		}
 	};
 	
